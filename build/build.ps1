ba88b8b483b0321d5ed0cdef211a0e28db44f577
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2021-07-27 18:42:55 +0100

    added a new second foramt

--- build/build.ps1
+++ build/build.ps1
@@ -11,25 +11,7 @@ Write-InfoLog "========================================================"
 Write-InfoLog "{ProjectTitle}" -PropertyValues $projectinfo.title
 $projectpath = Split-Path $project -Parent
 
-# Create compiled files
-$projectpathobj = Join-Path -Path $projectpath -ChildPath ".obj"
-if (!(Test-Path $projectpathobj))
-{
-    New-Item -itemType Directory -Path $projectpathobj
-}
-Write-DebugLog "projectpathobj: {projectpathobj}" -PropertyValues (Resolve-Path $projectpathobj -Relative)
-Write-DebugLog "Process header.hex"
-$objHeaderHexPath = (Join-Path -Path $projectpathobj -ChildPath "header.hex") 
-$headertexcontent = Get-Content (Join-Path -Path $buildRoot -ChildPath "header.tex")
-$headertexcontent | Out-File -FilePath $objHeaderHexPath
-Write-DebugLog "header.hex: {objHeaderHexPath}" -PropertyValues (Resolve-Path $objHeaderHexPath -Relative)
-Write-DebugLog "Process cover.hex"
-$objCoverHexPath = (Join-Path -Path $projectpathobj -ChildPath "cover.hex") 
-$covertexcontent = Get-Content (Join-Path -Path $buildRoot -ChildPath "cover.tex")
-$covertexcontent | Out-File -FilePath $objCoverHexPath
-Write-DebugLog "cover.hex: {objCoverHexPath}" -PropertyValues (Resolve-Path $objCoverHexPath -Relative)
-
-# Create compiled files
+# Create Output folder
 $projectOutputPath = Join-Path -Path $projectpath -ChildPath "bin"
 if (!(Test-Path $projectOutputPath))
 {
@@ -37,23 +19,59 @@ if (!(Test-Path $projectOutputPath))
 } else {
     Remove-Item $projectOutputPath\*.*
 }
-
 Write-DebugLog "projectOutputPath: {projectOutputPath}" -PropertyValues (Resolve-Path $projectOutputPath -Relative)
+# Create compiled files
+$projectpathobj = Join-Path -Path $projectpath -ChildPath ".obj"
+if (!(Test-Path $projectpathobj))
+{
+    New-Item -itemType Directory -Path $projectpathobj
+}
+Write-DebugLog "projectpathobj: {projectpathobj}" -PropertyValues (Resolve-Path $projectpathobj -Relative)
 
-Write-DebugLog "Calling Pandoc" 
-Set-Location $projectpath
+if ($projectinfo.assets -ne $null)
+{
+    foreach ($asset in $projectinfo.assets) 
+    {
+        Copy-Item -Path $asset -Destination $projectOutputPath
+    }
+}
 
-$FileSafeTitle = $projectinfo.title -replace " ", "-"
-& pandoc -f gfm -s --include-in-header $objHeaderHexPath --include-before-body $objCoverHexPath --toc --toc-depth=3 -V geometry:margin=1in -o (Join-Path -Path $projectOutputPath -ChildPath "$FileSafeTitle-v$version.pdf") $projectinfo.files
-Set-Location $repoRoot
-Write-InfoLog "=====BUILD PROJECT END {projectpathr}" -PropertyValues $projectpathr
+if ( $projectinfo.buildoption -eq "Markdown2PDF")
+{
+    Write-DebugLog "Process header.hex"
+    $objHeaderHexPath = (Join-Path -Path $projectpathobj -ChildPath "header.hex") 
+    $headertexcontent = Get-Content (Join-Path -Path $buildRoot -ChildPath "header.tex")
+    $headertexcontent | Out-File -FilePath $objHeaderHexPath
+    Write-DebugLog "header.hex: {objHeaderHexPath}" -PropertyValues (Resolve-Path $objHeaderHexPath -Relative)
+    Write-DebugLog "Process cover.hex"
+    $objCoverHexPath = (Join-Path -Path $projectpathobj -ChildPath "cover.hex") 
+    $covertexcontent = Get-Content (Join-Path -Path $buildRoot -ChildPath "cover.tex")
+    $covertexcontent | Out-File -FilePath $objCoverHexPath
+    Write-DebugLog "cover.hex: {objCoverHexPath}" -PropertyValues (Resolve-Path $objCoverHexPath -Relative)
+    
+    Write-DebugLog "Calling Pandoc" 
+    Set-Location $projectpath
+    
+    $FileSafeTitle = $projectinfo.title -replace " ", "-"
+    & pandoc -f gfm -s --include-in-header $objHeaderHexPath --include-before-body $objCoverHexPath --toc --toc-depth=3 -V geometry:margin=1in -o (Join-Path -Path $projectOutputPath -ChildPath "$FileSafeTitle-v$version.pdf") $projectinfo.files
+    Set-Location $repoRoot
+    Write-InfoLog "=====BUILD PROJECT END {projectpathr}" -PropertyValues $projectpathr
+    
+    
+    #Remove-Item -path $mypath\ -Filter *text2pdf* -WhatIf
+    
+    #$json = Get-Content "$mypath\configuration.json" | Out-String | ConvertFrom-Json
+    
+    #https://dev.to/learnbyexample/customizing-pandoc-to-generate-beautiful-pdfs-from-markdown-3lbj
+    #https://pandoc.org/lua-filters.html#examples
+    #https://stackoverflow.com/questions/48169995/pandoc-how-to-link-to-a-section-in-another-markdown-file
+    #https://stackoverflow.com/questions/60008898/pandoc-separate-table-of-contents-for-each-section    
+
+}
+
+if ( $projectinfo.buildoption -eq "PowerPoint")
+{
+    #
+}
 
 
-#Remove-Item -path $mypath\ -Filter *text2pdf* -WhatIf
-
-#$json = Get-Content "$mypath\configuration.json" | Out-String | ConvertFrom-Json
-
-#https://dev.to/learnbyexample/customizing-pandoc-to-generate-beautiful-pdfs-from-markdown-3lbj
-#https://pandoc.org/lua-filters.html#examples
-#https://stackoverflow.com/questions/48169995/pandoc-how-to-link-to-a-section-in-another-markdown-file
-#https://stackoverflow.com/questions/60008898/pandoc-separate-table-of-contents-for-each-section
