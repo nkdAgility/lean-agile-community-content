776dc317c10ca873ed8e94f359ccb25f07e2ea85
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2021-08-19 11:32:06 +0100

    Created build, move to root buin!

--- build/build.ps1
+++ build/build.ps1
@@ -1,61 +1,96 @@
-param([Parameter(Mandatory=$true)] [string]$project, [Parameter(Mandatory=$true)] [string]$version)
+param([Parameter(Mandatory=$true)] [string]$project, [Parameter(Mandatory=$true)] [string]$version, [Parameter(Mandatory=$false)] [boolean]$rebuild = $false)
 
 Set-Location $PSScriptRoot
 . .\include-logging.ps1
-Write-InfoLog "========================================================"
-Write-InfoLog "Build {project}  v{version}" -PropertyValues (Resolve-Path $project -Relative), $version
+Write-InfoLog "-----------------------------------------------------------"
+Write-InfoLog "|- Build {project}  v{version}" -PropertyValues (Resolve-Path $project -Relative), $version
 . .\include-variables.ps1
-Write-InfoLog "========================================================"
+
  # Get json
  $projectinfo = Get-Content $project | Out-String | ConvertFrom-Json
-Write-InfoLog "{ProjectTitle}" -PropertyValues $projectinfo.title
+Write-InfoLog " |- {ProjectTitle}" -PropertyValues $projectinfo.title
 $projectpath = Split-Path $project -Parent
 
 # Create Output folder
+Write-InfoLog " |- Manage BIN Folder"
 $projectOutputPath = Join-Path -Path $projectpath -ChildPath "bin"
 if (!(Test-Path $projectOutputPath))
 {
+    Write-DebugLog "  |- Creating BIN"
     New-Item -itemType Directory -Path $projectOutputPath
 } else {
-    Remove-Item $projectOutputPath\*.*
+    if ($rebuild)
+    {
+        Write-DebugLog "  |- Cleaning BIN"
+        Remove-Item $projectOutputPath\*.*
+    }    
 }
-Write-DebugLog "projectOutputPath: {projectOutputPath}" -PropertyValues (Resolve-Path $projectOutputPath -Relative)
+Write-DebugLog "  |- projectOutputPath: {projectOutputPath}" -PropertyValues (Resolve-Path $projectOutputPath -Relative)
+
 # Create compiled files
+Write-InfoLog " |-- Manage .obj Folder"
 $projectpathobj = Join-Path -Path $projectpath -ChildPath ".obj"
 if (!(Test-Path $projectpathobj))
 {
+    Write-DebugLog "  |- Creating .obj"
     New-Item -itemType Directory -Path $projectpathobj
+} else {
+    if ($rebuild)
+    {
+        Write-DebugLog "  |- Cleaning .obj"
+        Remove-Item $projectOutputPath\*.*
+    }    
 }
-Write-DebugLog "projectpathobj: {projectpathobj}" -PropertyValues (Resolve-Path $projectpathobj -Relative)
+Write-DebugLog "  |- projectpathobj: {projectpathobj}" -PropertyValues (Resolve-Path $projectpathobj -Relative)
 
+#Add Assets
+Write-InfoLog " |- Move assets to bin"
 if ($projectinfo.assets -ne $null)
 {
     foreach ($asset in $projectinfo.assets) 
     {
+        Write-DebugLog "  |- Asset: {asset}" -PropertyValues $asset
         Copy-Item -Path $asset -Destination $projectOutputPath
     }
 }
-Write-DebugLog "buildoption: {buildoption}" -PropertyValues $projectinfo.buildoption
+
+Write-InfoLog " |- Build {buildoption} " -PropertyValues $projectinfo.buildoption
 if ( $projectinfo.buildoption -eq "Markdown2PDF")
 {
-    Write-DebugLog "Process header.hex"
+    Write-InfoLog "  |- Process header.hex"
     $objHeaderHexPath = (Join-Path -Path $projectpathobj -ChildPath "header.hex") 
     $headertexcontent = Get-Content (Join-Path -Path $buildRoot -ChildPath "header.tex")
     $headertexcontent | Out-File -FilePath $objHeaderHexPath
-    Write-DebugLog "header.hex: {objHeaderHexPath}" -PropertyValues (Resolve-Path $objHeaderHexPath -Relative)
-    Write-DebugLog "Process cover.hex"
+    Write-DebugLog "   |- header.hex: {objHeaderHexPath}" -PropertyValues (Resolve-Path $objHeaderHexPath -Relative)
+    Write-InfoLog "  |- Process cover.hex"
     $objCoverHexPath = (Join-Path -Path $projectpathobj -ChildPath "cover.hex") 
     $covertexcontent = Get-Content (Join-Path -Path $buildRoot -ChildPath "cover.tex")
     $covertexcontent | Out-File -FilePath $objCoverHexPath
-    Write-DebugLog "cover.hex: {objCoverHexPath}" -PropertyValues (Resolve-Path $objCoverHexPath -Relative)
+    Write-DebugLog "   |- cover.hex: {objCoverHexPath}" -PropertyValues (Resolve-Path $objCoverHexPath -Relative)
     
-    Write-DebugLog "Calling Pandoc" 
+    Write-InfoLog " |- Calling Pandoc" 
     Set-Location $projectpath
     
-    $FileSafeTitle = $projectinfo.title -replace " ", "-"
-    & pandoc -f gfm -s --include-in-header $objHeaderHexPath --include-before-body $objCoverHexPath --toc --toc-depth=3 -V geometry:margin=1in -o (Join-Path -Path $projectOutputPath -ChildPath "$FileSafeTitle-v$version.pdf") $projectinfo.files
+    $FileSafeTitle = $projectinfo.title -replace " ", "-";
+    $outputFileName = "$FileSafeTitle-v$version.pdf";
+    $outputFilePath = (Join-Path -Path $projectOutputPath -ChildPath $outputFileName)
+    Write-DebugLog "  |- & pandoc -f gfm -s --include-in-header $objHeaderHexPath --include-before-body $objCoverHexPath --toc --toc-depth=3 -V geometry:margin=1in -o $outputFilePath $($projectinfo.files)"
+    if ($rebuild -and (!Test-Path($outputFilePath)))
+    {
+        & pandoc -f gfm -s --include-in-header $objHeaderHexPath --include-before-body $objCoverHexPath --toc --toc-depth=3 -V geometry:margin=1in -o $outputFilePath $projectinfo.files
+    }
+
+    
     Set-Location $repoRoot
-    Write-InfoLog "=====BUILD PROJECT END {projectpathr}" -PropertyValues $projectpathr
+
+    $bindir = Get-ChildItem -Path $projectOutputPath
+    Write-InfoLog " |- Bin now contains {ProjectTitle}" -PropertyValues $bindir.count;
+    foreach ($item in $bindir)
+    {
+        Write-InfoLog "  |- Item: {Item}" -PropertyValues $item.Name;
+    }
+
+    Write-InfoLog " |- BUILD PROJECT END {projectpathr}" -PropertyValues (Resolve-Path $project -Relative)
     
     
     #Remove-Item -path $mypath\ -Filter *text2pdf* -WhatIf
--- build/buildall.ps1
+++ build/buildall.ps1
@@ -1,5 +1,5 @@
 param([Parameter(Mandatory=$false)] [string]$version)
-
+Clear-Host;
 if (($version -eq $null) -or ($version -eq ""))
 {
     $version = "0.0.0"
@@ -26,9 +26,38 @@ foreach ($project in $projects)
     Write-InfoLog "********************************************************"
     Write-InfoLog "Running build.ps1 with {count} projects to build" -PropertyValues (Resolve-Path $project.FullName -Relative)
     
-    & .\build\build.ps1 -project $project.FullName -version $version
+    & .\build\build.ps1 -project $project.FullName -version $version -rebuild $false
     Write-InfoLog "********************************************************"
+    
 }
 Write-InfoLog "********************************************************"
 Write-InfoLog "Completed build of {count} projects" -PropertyValues $projects.count
+Write-InfoLog "********************************************************"
+
+# Create Output folder
+Write-InfoLog "|- Manage Global BIN Folder"
+$OutputPath = Join-Path -Path $repoRoot -ChildPath "bin"
+if (!(Test-Path $OutputPath))
+{
+    Write-DebugLog " |- Creating BIN"
+    New-Item -itemType Directory -Path $OutputPath
+} else {
+    if ($rebuild)
+    {
+        Write-DebugLog " |- Cleaning BIN"
+        Remove-Item $OutputPath\*.*
+    }    
+}
+Write-DebugLog "  |- OutputPath: {OutputPath}" -PropertyValues (Resolve-Path $OutputPath -Relative)
+
+
+$outputfiles = Get-ChildItem -path (Join-Path -Path $repoRoot -ChildPath "source") -recurse ($source) -File | Where-Object {$_.PSParentPath -match "bin"}
+Write-InfoLog "|- Found {count} output files" -PropertyValues $outputfiles.count;
+foreach ($outfile in $outputfiles)
+{
+    Write-InfoLog " |- Moving {file} to bin" -PropertyValues $outfile.Name
+    Copy-Item $outfile -Destination $OutputPath
+}
+Write-InfoLog "********************************************************"
+Write-InfoLog "Completed"
 Write-InfoLog "********************************************************"
